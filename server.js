c9314720139b9cc7b2ce5968e0f8789311c40d85
davipedro0505 <davipedrosilvaalves@gmail.com>
2025-10-30 12:47:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,17 +5,60 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use = (express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.engine("handlebars", exphbs.engine ({defaultLayout: false}));
 app.set ("view engine", "handlebars");
 
+//Arrays de Pessoas
 let pessoas = [
     {id: 1, nome: "Pessoa 1"},
     {id: 2, nome: "Pessoa 2"},
     {id: 3, nome: "Pessoa 3"},
 ];
 
-app.listen(port,()=>{
-    console.log('servidor em execução: http://localhost:$(port)')
+
+//Principal
+app.get('/', (req, res) => { 
+    res.render('home')  
 });
+
+//Listar Pessoas
+app.get("/pessoas", (req, res) => {
+    res.render('listarPessoas', {pessoas});
+});
+
+app.get('/pessoas/nova', (req, res) => { 
+    res.render('cadastrarPessoa')  
+});
+
+app.post('/pessoas', (req, res) =>{
+    const nome = req.body.nome;
+    
+
+    const novaPessoa = {
+        id: pessoas.length + 1,
+        nome: nome
+    };
+
+    pessoas.push(novaPessoa);
+
+    res.render('listarPessoas', { pessoas});
+});
+
+app.get('/pessoas/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(p => p.id ===id);
+
+    if(pessoa){
+        res.render('detalharPessoa', {pessoa});
+    }else{
+        res.status(404).send('Pessoa não encontrada!');
+    }
+});
+
+
+app.listen(port,()=>{
+    console.log(`servidor em execução: http://localhost:${port}`)
+});
+
